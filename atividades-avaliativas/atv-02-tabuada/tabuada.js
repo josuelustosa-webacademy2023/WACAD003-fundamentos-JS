3d527912c92003565bad467e63a2015fb0a402b9
Josué Lustosa <josuelustosa.job@gmail.com>
2023-04-13 00:17:49 -0400

    feat: versao submetida no colabweb

--- atividades-avaliativas/atv-02-tabuada/tabuada.js
+++ atividades-avaliativas/atv-02-tabuada/tabuada.js
@@ -1,30 +1,31 @@
 function gerarTabuada() {
-//   console.log('CLICOUUUUUUUU');
+  const cardTabuada = document.querySelector(".card-tabuada");
+  cardTabuada.removeAttribute("hidden");
 
   let inputQuantTabuada = parseInt(
-    document.getElementById('quant-tabuada').value
+    document.getElementById("quant-tabuada").value
   );
 
-  numerosDeMultiplicacao = [];
+  const tabuada = document.getElementById("tabuada");
 
-  for (let i = 1; i <= 10; i++) {
-    numerosDeMultiplicacao.push(i);
-    console.log(numerosDeMultiplicacao);
+  if (inputQuantTabuada > 0) {
+    let tabuadaHtml = "";
+
+    for (let u = 1; u <= inputQuantTabuada; u++) {
+      tabuadaHtml += "<h2>Tabuada do " + u + "</h2>";
+      tabuadaHtml += "<br/>";
+      tabuadaHtml += "<ul>";
+
+      for (let i = 1; i <= 10; i++) {
+        tabuadaHtml += "<li>" + i + " x " + u + " = " + i * u + "</li>";
+      }
+
+      tabuadaHtml += "</ul>";
+    }
+
+    tabuada.innerHTML = tabuadaHtml;
+  } else {
+    alert("Todo número multiplicado por 0 é 0.");
+    cardTabuada.setAttribute("hidden", "");
   }
-
-  const tabuada = `
-    <ul>
-        ${numerosDeMultiplicacao
-          .map(
-            (item) =>
-              `<li>${item} x ${inputQuantTabuada} = ${
-                item * inputQuantTabuada
-              }  </li>`
-          )
-          .join('')}
-    </ul>
-  `;
-
-  const criarTabuada = document.querySelector('.card-tabuada');
-  criarTabuada.innerHTML = tabuada;
 }
