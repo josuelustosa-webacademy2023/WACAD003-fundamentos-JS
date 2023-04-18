981ec0990cbdd26a0e971b5e11a904673cf99cbd
Josué Lustosa <josuelustosa.job@gmail.com>
2023-04-17 23:32:36 -0400

    refactor: refatorando funcao soma com Function Expression

--- atividades-avaliativas/atv-01-calculadora/calculadora.js
+++ atividades-avaliativas/atv-01-calculadora/calculadora.js
@@ -1,53 +1,61 @@
 function calcular() {
-  let valor1 = parseInt(document.getElementById('valor1').value);
+  let valor1 = parseInt(document.getElementById("valor1").value);
 
-  let valor2 = parseInt(document.getElementById('valor2').value);
-
-  let resultado = valor1 + valor2;
-
-  document.getElementById('resultado').innerHTML = `= ${resultado}`;
-}
-
-function soma() {
-  let valor1 = parseInt(document.getElementById('valor1').value);
-
-  let valor2 = parseInt(document.getElementById('valor2').value);
+  let valor2 = parseInt(document.getElementById("valor2").value);
 
   let resultado = valor1 + valor2;
 
-  document.getElementById('operacao').innerHTML = `+`;
-  document.getElementById('resultado').innerHTML = `= ${resultado}`;
+  document.getElementById("resultado").innerHTML = `= ${resultado}`;
 }
+
+// Function Expression
+
+function soma(valor1, valor2) {
+  var resultadoSomaParam = valor1 + valor2;
+  return resultadoSomaParam;
+}
+
+var form = document.querySelector("form");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  var valor1 = parseInt(document.getElementById("valor1").value);
+  var valor2 = parseInt(document.getElementById("valor2").value);
+  var resultadoSoma = soma(valor1, valor2);
+
+  document.getElementById("resultado").innerHTML = `= ${resultadoSoma}`;
+});
+
+// ------------------------------------------------------------
 
 function subtracao() {
-  let valor1 = parseInt(document.getElementById('valor1').value);
+  let valor1 = parseInt(document.getElementById("valor1").value);
 
-  let valor2 = parseInt(document.getElementById('valor2').value);
+  let valor2 = parseInt(document.getElementById("valor2").value);
 
   let resultado = valor1 - valor2;
 
-  document.getElementById('operacao').innerHTML = `-`;
-  document.getElementById('resultado').innerHTML = `= ${resultado}`;
+  document.getElementById("operacao").innerHTML = `-`;
+  document.getElementById("resultado").innerHTML = `= ${resultado}`;
 }
 
 function multiplicacao() {
-  let valor1 = parseInt(document.getElementById('valor1').value);
+  let valor1 = parseInt(document.getElementById("valor1").value);
 
-  let valor2 = parseInt(document.getElementById('valor2').value);
+  let valor2 = parseInt(document.getElementById("valor2").value);
 
   let resultado = valor1 * valor2;
 
-  document.getElementById('operacao').innerHTML = `*`;
-  document.getElementById('resultado').innerHTML = `= ${resultado}`;
+  document.getElementById("operacao").innerHTML = `*`;
+  document.getElementById("resultado").innerHTML = `= ${resultado}`;
 }
 
 function divisao() {
-  let valor1 = parseInt(document.getElementById('valor1').value);
+  let valor1 = parseInt(document.getElementById("valor1").value);
 
-  let valor2 = parseInt(document.getElementById('valor2').value);
+  let valor2 = parseInt(document.getElementById("valor2").value);
 
   let resultado = valor1 / valor2;
 
-  document.getElementById('operacao').innerHTML = `/`;
-  document.getElementById('resultado').innerHTML = `= ${resultado}`;
+  document.getElementById("operacao").innerHTML = `/`;
+  document.getElementById("resultado").innerHTML = `= ${resultado}`;
 }
